097796b1bbe7505b808990c6827e0cb5fc03e0d2
Marius Magureanu <mariusmagureanu@live.dk>
2021-04-26 17:14:36 +0200

    Added a first draft of sse support

--- src/pexip/pexip.go
+++ src/pexip/pexip.go
@@ -4,8 +4,11 @@
 package pexip
 
 import (
+	"bufio"
 	"bytes"
+	"context"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"time"
@@ -37,8 +40,23 @@ const (
 var (
 	pexipHost       string
 	client          *http.Client
+	sseClient       *http.Client
 	refreshInterval time.Duration
+
+	headerID    = []byte("id:")
+	headerData  = []byte("data:")
+	headerEvent = []byte("event:")
+	headerRetry = []byte("retry:")
 )
+
+// Event is a type corresponding to a server
+// sent event sent by Pexip.
+type Event struct {
+	ID    string
+	Data  string
+	Event string
+	Retry string
+}
 
 // InitConfStore initializes a new ConferenceStore.
 func InitConfStore() *ConferenceStore {
@@ -52,7 +70,7 @@ func InitConfStore() *ConferenceStore {
 func InitTokenStore() *TokenStore {
 	ts := TokenStore{}
 	ts.store = make(map[string]string)
-	ts.doneListeners = make(map[string](chan bool))
+	ts.doneListeners = make(map[string]chan bool)
 
 	return &ts
 }
@@ -64,8 +82,8 @@ func InitParticipantStore() *ParticipantStore {
 	return &ps
 }
 
-// InitPexipClient intializes a new http client used
-// for all comunication against the Pexip api.
+// InitPexipClient initializes a new http client used
+// for all communication against the Pexip api.
 func InitPexipClient(host string, timeout time.Duration, maxConns, maxIdleConns int, tokenRefresh time.Duration) error {
 
 	pexipHost = host
@@ -78,6 +96,10 @@ func InitPexipClient(host string, timeout time.Duration, maxConns, maxIdleConns 
 
 	client = &http.Client{
 		Timeout:   timeout,
+		Transport: t,
+	}
+
+	sseClient = &http.Client{
 		Transport: t,
 	}
 
@@ -129,7 +151,12 @@ func doRequest(method, url, token, pin string, payload []byte) ([]byte, error) {
 		return out, err
 	}
 
-	defer resp.Body.Close()
+	defer func(body io.ReadCloser) {
+		err := body.Close()
+		if err != nil {
+			logger.Error(err)
+		}
+	}(resp.Body)
 
 	out, err = ioutil.ReadAll(resp.Body)
 
@@ -138,4 +165,108 @@ func doRequest(method, url, token, pin string, payload []byte) ([]byte, error) {
 	}
 
 	return out, nil
+}
+
+func sse(roomName, token string) error {
+	logger.Debug("starting sse for room", roomName)
+	sseURL := fmt.Sprintf("%s/%s/%s/%s", pexipHost, "api/client/v2/conferences", roomName, "events")
+	req, err := http.NewRequest(http.MethodGet, sseURL, nil)
+
+	if err != nil {
+		return err
+	}
+
+	ctx, _ := context.WithCancel(context.Background())
+
+	req = req.WithContext(ctx)
+
+	query := req.URL.Query()
+	query.Add("token", token)
+	req.URL.RawQuery = query.Encode()
+
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("Accept", "text/event-stream")
+	req.Header.Set("Connection", "keep-alive")
+
+	sseResp, err := sseClient.Do(req)
+
+	if err != nil {
+		logger.Error(err)
+		return err
+	}
+
+	defer func(body io.ReadCloser) {
+		err := body.Close()
+		if err != nil {
+			logger.Error(err)
+		}
+	}(sseResp.Body)
+
+	bodyReader := bufio.NewReader(sseResp.Body)
+
+	for {
+		msg, err := bodyReader.ReadBytes('\n')
+		if err != nil {
+			logger.Error(err)
+			break
+		}
+
+		_, err = processEvent(msg)
+
+		if err != nil {
+			logger.Error(err)
+			break
+		}
+	}
+
+	return nil
+}
+
+func processEvent(msg []byte) (Event, error) {
+	var (
+		e     Event
+		id    []byte
+		data  []byte
+		event []byte
+		retry []byte
+	)
+
+	if len(msg) == 0 {
+		return e, fmt.Errorf("event message was empty")
+	}
+
+	bytes.Replace(msg, []byte("\n\r"), []byte("\n"), -1)
+	for _, line := range bytes.FieldsFunc(msg, func(r rune) bool { return r == '\n' || r == '\r' }) {
+		switch {
+		case bytes.HasPrefix(line, headerID):
+			id = append([]byte(nil), trimHeader(len(headerID), line)...)
+		case bytes.HasPrefix(line, headerData):
+			data = append(append(trimHeader(len(headerData), line), data[:]...), byte('\n'))
+		case bytes.Equal(line, bytes.TrimSuffix(headerData, []byte(":"))):
+			data = append(data, byte('\n'))
+		case bytes.HasPrefix(line, headerEvent):
+			event = append([]byte(nil), trimHeader(len(headerEvent), line)...)
+		case bytes.HasPrefix(line, headerRetry):
+			retry = append([]byte(nil), trimHeader(len(headerRetry), line)...)
+		default:
+		}
+	}
+
+	e.ID = string(id)
+	e.Event = string(event)
+	e.Data = string(bytes.TrimSuffix(data, []byte("\n")))
+	e.Retry = string(retry)
+
+	return e, nil
+}
+
+func trimHeader(size int, data []byte) []byte {
+	data = data[size:]
+	if data[0] == 32 {
+		data = data[1:]
+	}
+	if len(data) > 0 && data[len(data)-1] == 10 {
+		data = data[:len(data)-1]
+	}
+	return data
 }
